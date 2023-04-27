c66cecb9b5842e999c5d498754c3db254ad63401
Rajan-abjima <125352988+Rajan-abjima@users.noreply.github.com>
2023-04-27 20:15:09 +0530

    27-04-23

---
+++ CityInfo/CityInfo.API/Controllers/PointsOfInterestController.cs
@@ -0,0 +1,71 @@
+﻿using CityInfo.API.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CityInfo.API.Controllers
+{
+    [Route("api/cities/{cityId}/pointsofinterest")]
+    [ApiController]
+    public class PointsOfInterestController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<IEnumerable<PointOfInterestDto>> GetPointsOfInterest(int cityId)
+        {
+            var city = CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == cityId);
+
+            if (city == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(city.PointsOfInterest);
+        }
+
+        [HttpGet("{pointofinterestid}")]
+        public ActionResult<PointOfInterestDto> GetPointOfInterest(
+            int cityId, int pointOfInterestId)
+        {
+            var city = CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == cityId);
+
+            if (city == null)
+            {
+                return NotFound();
+            }
+
+            //point of interest
+            var pointOfInterest = city.PointsOfInterest.FirstOrDefault(c => c.Id == pointOfInterestId);
+
+            if (pointOfInterest == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(pointOfInterest);
+        }
+
+        ////////Error Code dated 27-04-23///////
+
+        //[HttpPost]
+        //public ActionResult<PointOfInterestDto> CreatePointOfInterest(int cityId, PointOfInterestForCreatonDto pointOfInterest)
+        //{
+        //    var city = CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == cityId);
+
+        //    if (city == null)
+        //    {
+        //        return NotFound();
+        //    }
+
+        //    var maxPointOfInterestId = CitiesDataStore.Current.Cities.SelectMany(
+        //        c => c.PointsOfInterest).Max(p => p.Id);
+
+        //    var finalPointOfInterest = new PointOfInterestForCreatonDto()
+        //    {
+        //        Id = ++maxPointOfInterestId,
+        //        Name = pointOfInterest.Name,
+        //        Description = pointOfInterest.Description
+        //    };
+
+        //    city.PointsOfInterest.Add(finalPointOfInterest);
+        //}
+    }
+}
