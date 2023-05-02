136e68b2aa9b469a7b501fbb4180fc8fd60d35c0
Rajan-abjima <125352988+Rajan-abjima@users.noreply.github.com>
2023-05-02 20:02:21 +0530

    2-5-23

---
+++ CityInfo/CityInfo.API/DBContext/CityInfoContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace CityInfo.API.DBContext
+{
+    public class CityInfoContext : DBContext
+    {
+        public DbSet<City> Cities { get; set; } = null!;
+        public DbSet<PointOfInterest> PointsOfInterest { get; set; } = null!;
+
+        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        //{
+        //    optionsBuilder.UseSqlite("connectionstring");
+        //    base.OnConfiguring(optionsBuilder);
+        //}
+    }
+}
---
+++ CityInfo/CityInfo.API/Services/CloudMailService.cs
@@ -0,0 +1,22 @@
+﻿namespace CityInfo.API.Services
+{
+    public class CloudMailService : IMailService
+    {
+        private readonly string _mailTo = string.Empty;
+        private readonly string _mailFrom = string.Empty;
+
+        public CloudMailService(IConfiguration configuration)
+        {
+            _mailTo = configuration["mailSettngs:mailToAddress"];
+            _mailFrom = configuration["mailSettngs:mailFromAddress"];
+        }
+
+        public void Send(string subject, string message)
+        {
+            //demo way to mailoutput in console
+            Console.WriteLine($"Mail from {_mailFrom} to {_mailTo}," + $"with {nameof(CloudMailService)}.");
+            Console.WriteLine($"Subject: {subject}");
+            Console.WriteLine($"Message: {message}");
+        }
+    }
+}
