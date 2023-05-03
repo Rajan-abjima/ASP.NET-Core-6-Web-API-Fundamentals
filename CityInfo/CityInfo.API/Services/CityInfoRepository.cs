957b42d8da271bd8c041bd7b0b286bbac07a84c4
Rajan-abjima <125352988+Rajan-abjima@users.noreply.github.com>
2023-05-03 19:59:51 +0530

    3-5-23

---
+++ CityInfo/CityInfo.API/Services/CityInfoRepository.cs
@@ -0,0 +1,42 @@
+﻿using CityInfo.API.DBContext;
+using CityInfo.API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CityInfo.API.Services
+{
+    public class CityInfoRepository : ICityInfoRepository
+    {
+        private readonly CityInfoContext _context;
+
+        public CityInfoRepository(CityInfoContext context)
+        {
+            _context = context ?? throw new ArgumentNullException(nameof(context));
+        }
+        public async Task<IEnumerable<City>> GetCitiesAsync()
+        {
+            return await _context.Cities.OrderBy(c => c.Name).ToListAsync();
+        }
+
+        public async Task<City?> GetCityAsync(int cityId, bool includePointsOfInterest)
+        {
+            if (includePointsOfInterest)
+            {
+                return await _context.Cities.Include(c => c.PointsOfInterests).Where(c => c.Id == cityId).FirstOrDefaultAsync();
+            }
+                return await _context.Cities.Where(c => c.Id == cityId).FirstOrDefaultAsync();
+        }
+        public async Task<PointOfInterest?> GetPointOfInterestForCityAsync(int cityId, int pointOfInterestId)
+        {
+            return await _context.PointsOfInterest
+                .Where(p => p.Id == cityId && p.Id == pointOfInterestId)
+                .FirstOrDefaultAsync();
+        }
+
+        public async Task<IEnumerable<PointOfInterest>> GetPointOfInterestForCityAsync(int cityId)
+        {
+            return await _context.PointsOfInterest
+                .Where(p => p.CityId == cityId).ToListAsync();
+        }
+
+    }
+}
---
+++ CityInfo/CityInfo.API/Services/LocalMailService.cs
@@ -0,0 +1,22 @@
+﻿namespace CityInfo.API.Services
+{
+    public class LocalMailService : IMailService
+    {
+        private readonly string _mailTo = string.Empty;
+        private readonly string _mailFrom = string.Empty;
+
+        public LocalMailService(IConfiguration configuration)
+        {
+            _mailTo = configuration["mailSettngs:mailToAddress"];
+            _mailFrom = configuration["mailSettngs:mailFromAddress"];
+        }
+
+        public void Send(string subject, string message)
+        {
+            //demo way to mailoutput in console
+            Console.WriteLine($"Mail from {_mailFrom} to {_mailTo}," + $"with {nameof(LocalMailService)}.");
+            Console.WriteLine($"Subject: {subject}");
+            Console.WriteLine($"Message: {message}");
+        }
+    }
+}
