f0851479a8f2ca913b6eb090e80d413d6fde48dc
Rajan-abjima <125352988+Rajan-abjima@users.noreply.github.com>
2023-05-11 19:16:11 +0530

    11-05-23

--- CityInfo/CityInfo.API/Controllers/CitiesController.cs
+++ CityInfo/CityInfo.API/Controllers/CitiesController.cs
@@ -10,7 +10,8 @@ namespace CityInfo.API.Controllers
 {
     [ApiController]
     [Authorize]
-    [Route("api/cities")]
+    [ApiVersion("1.0")]
+    [Route("api/v.{version:apiVersion}/cities")]
     public class CitiesController : ControllerBase
     {
         private readonly ICityInfoRepository _cityInfoRepository;
@@ -58,7 +59,17 @@ namespace CityInfo.API.Controllers
          
         }
 
+        /// <summary>
+        /// Get a City by Id
+        /// </summary>
+        /// <param name="id">The Id of city to get</param>
+        /// <param name="includePointsOfInterest">Whether or not to include the points of interest</param>
+        /// <returns>An IActionResult</returns>
+        /// <response code="200">Return the requested city</response>
         [HttpGet("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
         public async Task<IActionResult> GetCity(
             int id, bool includePointsOfInterest = false)
         {
--- CityInfo/CityInfo.API/Models/CityWithoutPointsOfInterestDto.cs
+++ CityInfo/CityInfo.API/Models/CityWithoutPointsOfInterestDto.cs
@@ -1,9 +1,21 @@
 ﻿namespace CityInfo.API.Models
 {
+    /// <summary>
+    /// A DTO fro a city without points of Interest
+    /// </summary>
     public class CityWithoutPointsOfInterestDto
     {
+        /// <summary>
+        /// Th e id of city
+        /// </summary>
         public int Id { get; set; }
+        /// <summary>
+        /// The name of city
+        /// </summary>
         public string Name { get; set; } = string.Empty;
+        /// <summary>
+        /// The description of city
+        /// </summary>
         public string? Description { get; set; }
     }
 }
